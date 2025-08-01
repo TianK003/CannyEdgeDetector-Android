420a7ea6c577a018161a2580b5e58ce4e9f29b8f
TianK003 <tian.kljucanin@gmail.com>
2025-08-01 22:32:22 +0200

    Use the camera screen composable

--- app/src/main/java/mobile/cannyedge/MainActivity.kt
+++ app/src/main/java/mobile/cannyedge/MainActivity.kt
@@ -11,37 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import mobile.cannyedge.ui.screens.CameraScreen
 import mobile.cannyedge.ui.theme.CannyEdgeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CannyEdgeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CameraScreen(modifier = Modifier.fillMaxSize())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CannyEdgeTheme {
-        Greeting("Android")
     }
 }
