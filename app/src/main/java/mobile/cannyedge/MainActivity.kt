d9b235a72b9b3cbff916deb742c8fac5af45554e
TianK003 <tian.kljucanin@gmail.com>
2025-08-28 00:17:37 +0200

    Added navigation between screens

--- app/src/main/java/mobile/cannyedge/MainActivity.kt
+++ app/src/main/java/mobile/cannyedge/MainActivity.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import mobile.cannyedge.ui.screens.CameraScreen
 import mobile.cannyedge.ui.theme.CannyEdgeTheme
 import org.opencv.android.OpenCVLoader
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import mobile.cannyedge.ui.screens.HomeScreen
+import mobile.cannyedge.ui.screens.LivePreviewScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +23,29 @@ class MainActivity : ComponentActivity() {
                 if (OpenCVLoader.initLocal()) {
                     Log.d("OpenCV", "OpenCV loaded successfully")
                 }
-                CameraScreen(modifier = Modifier.fillMaxSize())
+
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable("home") {
+                        HomeScreen(
+                            onNavigateToCamera = { navController.navigate("camera") },
+                            onNavigateToLivePreview = { navController.navigate("livepreview") }
+                        )
+                    }
+                    composable("camera") {
+                        CameraScreen(
+                            onBack = { navController.popBackStack() }
+                        )
+                    }
+                    composable("livepreview") {
+                        LivePreviewScreen(
+                            onBack = { navController.popBackStack() }
+                        )
+                    }
+                }
             }
         }
     }
